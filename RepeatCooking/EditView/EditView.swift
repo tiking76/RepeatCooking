92e0abe21b3fcf63d438b1ab392a2a8739a15aed
tiking76 <716yosiki1999@gmail.com>
2021-03-07 08:28:52 +0900

    create view

--- RepeatCooking/EditView/EditView.swift
+++ RepeatCooking/EditView/EditView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct EditView: View {
+    @State var date = Date()
+    @State var text: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("Camera")
+                .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+            Form(content: {
+                Section(header: Text("日時")) {
+                    DatePicker("", selection: $date,
+                        displayedComponents: .date)
+                        .frame(width: 230, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
+                Section(header: Text("point")) {
+                TextEditor(text: $text)
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                }
+            })
+        }
     }
 }
 
---
+++ RepeatCooking/EditView/TextEditor.swift
@@ -0,0 +1,49 @@
+//
+//  TextEditor.swift
+//  RepeatCooking
+//
+//  Created by tiking on 2021/03/07.
+//
+import SwiftUI
+// 複数行入力するためのTextField
+struct TextEditer: UIViewRepresentable {
+    @Binding var text: String
+
+    func makeUIView(context: Context) -> UITextView {
+        let view = UITextView()
+        view.delegate = context.coordinator
+        view.isScrollEnabled = true
+        view.isEditable = true
+        view.isUserInteractionEnabled = true
+        view.font = UIFont.systemFont(ofSize: 18)
+        return view
+    }
+
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        if uiView.text != text {
+            uiView.text = text
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator : NSObject, UITextViewDelegate {
+
+        var parent: TextEditer
+
+        init(_ textView: TextEditer) {
+            self.parent = textView
+        }
+
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            return true
+        }
+
+        func textViewDidChange(_ textView: UITextView) {
+            self.parent.text = textView.text
+        }
+    }
+}
+
