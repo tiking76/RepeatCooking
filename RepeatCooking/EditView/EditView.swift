299047b9a7d2fcd750dc7d2e3ff2daaa91b7f8b7
tiking76 <716yosiki1999@gmail.com>
2021-03-07 06:39:51 +0900

    create editView

---
+++ RepeatCooking/EditView/EditView.swift
@@ -0,0 +1,20 @@
+//
+//  EditView.swift
+//  RepeatCooking
+//
+//  Created by tiking on 2021/03/06.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView()
+    }
+}
